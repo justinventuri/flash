25d93173e708356be5469d91864ae637d63ebef4
justinventuri <justinventuri@gmail.com>
2016-02-12 10:17:08 -0500

    error

--- jv-flash-list.js
+++ jv-flash-list.js
@@ -1,14 +1,21 @@
 Template.FlashMessages.helpers({
   flashMessages: function() {
-    return Flash.collection.find();
+    return Flash.collection.find({type: Flash.messageTypes.INFO});
+  },
+  flashMessagesError: function() {
+    return Flash.collection.find({type: Flash.messageTypes.ERROR});
   }
 });
-
 
 Template.FlashMessage.rendered = function() {
   var message = this.data;
   Meteor.setTimeout(function() {
-    // Flash.collection.update(message._id, {$set: {seen: true}});
     Flash.collection.remove({_id: message._id});
   }, 3000 );
+};
+Template.FlashMessageError.rendered = function() {
+  var message = this.data;
+  Meteor.setTimeout(function() {
+    Flash.collection.remove({_id: message._id});
+  }, 5000 );
 };
--- jv-flash.js
+++ jv-flash.js
@@ -1,9 +1,30 @@
 Flash = {
   collection: new Meteor.Collection(null),
 
+  messageTypes: {
+    INFO: 0,
+    ERROR: 1
+  },
+
+  error: function(message) {
+    Flash.collection.upsert({
+      message: message
+    },{
+      $set: {
+        seen: false,
+        type: Flash.messageTypes.ERROR
+      }
+    });
+  },
   info: function(message) {
-    // Flash.collection.remove({})
-    Flash.collection.upsert({message: message}, {$set: {seen: false}});
+    Flash.collection.upsert({
+      message: message
+    },{
+      $set: {
+        seen: false,
+        type: Flash.messageTypes.INFO
+      }
+    });
   },
 
   clearSeen: function () {
